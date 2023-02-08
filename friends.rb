12f1e2602e99ab231e36bb9a0c9a32392f219d8d
Jesse Thomas <jgthomas3@gmail.com>
2023-02-07 19:03:20 -0700

    Add Teeter

--- friends.rb
+++ friends.rb
@@ -1,7 +1,10 @@
-bf = "Tim"
+bf = "John"
 
 if bf == "Max"
     puts "You're my best friend"
+elsif bf == "Teeter" 
+    puts "Teeter, you climb really well dawg"
 else 
     puts "#{bf}, you're my new friend"
 end
+
